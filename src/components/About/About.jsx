cc65db502df1c9008b3d155d3c04bc5301c887f6
AydanMurshud <aydinmurshud@gmail.com>
2022-08-29 20:32:21 +0300

    add responsivnes to all platforms

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -5,17 +5,11 @@ import './About.css'
 const About = () => {
     return (
         <div id='about' className='about'>
-            <div className="divider-top">
-                <svg width="100%" height="100%" id="svg" viewBox="0 0 1440 400" xmlns="http://www.w3.org/2000/svg"><path d="M 0,400 C 0,400 0,200 0,200 C 203.19999999999993,171.06666666666666 406.39999999999986,142.13333333333335 544,148 C 681.6000000000001,153.86666666666665 753.6000000000001,194.53333333333333 892,209 C 1030.3999999999999,223.46666666666667 1235.1999999999998,211.73333333333335 1440,200 C 1440,200 1440,400 1440,400 Z" stroke="none" stroke-width="0" fill="url(#gradient)" fill-opacity="1" class="shape-fill"></path></svg>
-            </div>
+            
             <div className="container about__container">
                 <h5>Who am I?</h5>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi praesentium, libero dolor velit iure aspernatur! Assumenda officia, voluptas quidem modi tenetur exercitationem labore eveniet eaque animi odio reiciendis sapiente suscipit sunt adipisci molestias cupiditate neque repudiandae. Voluptates illo assumenda est. Rem a aliquid atque odit nostrum minus totam vel architecto?</p>
             </div>
-            <div className="divider-bottom">
-                <svg width="100%" height="100%" id="svg" viewBox="0 0 1440 400" xmlns="http://www.w3.org/2000/svg"><path d="M 0,400 C 0,400 0,200 0,200 C 203.19999999999993,171.06666666666666 406.39999999999986,142.13333333333335 544,148 C 681.6000000000001,153.86666666666665 753.6000000000001,194.53333333333333 892,209 C 1030.3999999999999,223.46666666666667 1235.1999999999998,211.73333333333335 1440,200 C 1440,200 1440,400 1440,400 Z" stroke="none" stroke-width="0" fill="url(#gradient)" fill-opacity="1" class="shape-fill"></path></svg>
-            </div>
-            
         </div>
     )
 }
--- src/components/Education/Education.jsx
+++ src/components/Education/Education.jsx
@@ -4,6 +4,11 @@ import './Education.css'
 const Education = () => {
   return (
     <div id='education' className='education' id='education'>
+       <div class="divider">
+          <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+          <path d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z" className="shape-fill"></path>
+          </svg>
+        </div>
       <div className="container education__container">
         <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Beatae, magni. Sapiente recusandae minus aperiam, porro temporibus tempore libero hic ipsum quis natus suscipit necessitatibus quam a ad error molestias repellat eaque autem obcaecati id. Possimus cum nam dolores aut ipsum tempore soluta, minima voluptatibus ratione doloremque placeat nesciunt cupiditate consequatur quia facilis veritatis laudantium necessitatibus saepe repellat nostrum illum repudiandae labore iste. Corporis recusandae placeat reiciendis iusto debitis nesciunt, tenetur possimus nisi, voluptatibus quasi neque adipisci. Consectetur commodi dolor voluptatem esse adipisci vitae incidunt repellat placeat accusantium, blanditiis quam explicabo sequi doloribus. Dolor beatae ad perferendis hic provident delectus qui obcaecati, tempora deleniti iste impedit doloremque et cumque, vitae id illo, ab quo veniam inventore animi dicta numquam dolores. Nobis.</p>
       </div>
